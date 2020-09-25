206ccbd2d8a5648e7440790a2b54cbd404178824
Magnatikk <71604803+Magnatikk@users.noreply.github.com>
2020-09-25 14:48:10 +0200

    Add files via upload

---
+++ AlgoPRog/TP3Bis/DateMois.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class DateMois {
+	
+	public static void main(String[] args) {
+
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+
+		int jour, mois, annee, bis;
+
+			System.out.println("Rentrez le jour");
+				jour = Integer.parseInt(sc.nextLine());
+
+			System.out.println("Rentrez le mois");
+				mois = Integer.parseInt(sc.nextLine());	
+
+			System.out.println("Rentrez l'année");
+				annee = Integer.parseInt(sc.nextLine());	
+
+		if ( 0>jour || jour>31 || mois<0 || mois>12 || annee<1582)
+			System.out.println("Rentrez une date correcte");
+
+		else if (annee%4 == 0 && mois == 2 && jour > 29 )
+			System.out.println("Date incorrecte");
+		
+		else {
+			
+			if (mois == 1 || mois == 3 || mois == 5 || mois == 7 || mois == 8 || mois == 10 || mois == 12)
+				System.out.println("Date correcte");
+
+			else {
+
+				if (jour > 30)
+					System.out.println("Rentrez le bon jour");
+
+				else 
+					System.out.println("Date correcte");	
+			}
+		}
+	}
+}
+
